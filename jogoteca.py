bd9fa1c4b002498a0ab0233bca8de1b85696ba05
Raquel Reis <64446494+raquelcreis@users.noreply.github.com>
2022-11-26 14:16:44 -0300

    template.hmtl and extends in all html files

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template,request,redirect,session,flash
 
 class Jogo:
     def __init__(self,nome,categoria,console):
@@ -13,6 +13,7 @@ jogo3 = Jogo('Mortal Kombat','Luta','PS2')
 lista = [jogo1,jogo2,jogo3]
 
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 @app.route('/')
 def index():  
@@ -38,8 +39,11 @@ def login():
 @app.route('/autenticar',methods=['POST',])
 def autenticar():
     if 'alohomora' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + 'logado com sucesso!')
         return redirect('/')
     else:
+        flash('Usuário não logado')
         return redirect('/login')
 
 app.run(debug=True)
