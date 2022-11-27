179d4ada9d09967d603ecd31fe47e27e9a646c36
Raquel Reis <64446494+raquelcreis@users.noreply.github.com>
2022-11-26 21:10:36 -0300

    class Usuario

--- jogoteca.py
+++ jogoteca.py
@@ -12,6 +12,21 @@ jogo3 = Jogo('Mortal Kombat','Luta','PS2')
     
 lista = [jogo1,jogo2,jogo3]
 
+class Usuario:
+    def __init__(self,nome,nickname,senha):
+        self.nome=nome
+        self.nickname = nickname
+        self.senha = senha
+
+
+usuario1 = Usuario("Raquel Reis","Quel","alohomora")
+usuario2 = Usuario("Phoebe Buffay","Phoebe","smellycat")
+usuario3 = Usuario("Joey Tribbiani","Joey","howyoudoing")
+
+usuarios = { usuario1.nickname : usuario1 , 
+            usuario2.nickname : usuario2 , 
+            usuario3.nickname : usuario3}
+
 app = Flask(__name__)
 app.secret_key = 'alura'
 
@@ -43,15 +58,17 @@ def login():
 
 @app.route('/autenticar',methods=['POST',])
 def autenticar():
-    if 'alohomora' == request.form['senha']:
-        session['usuario_logado'] = request.form['usuario']
-        flash(session['usuario_logado'] + ' logado com sucesso!')
-        proxima_pagina = request.form['proxima']
-        return redirect(proxima_pagina)
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if request.form['senha'] == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname + ' logado com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
+        else:
+            flash('Usuário não logado')
+            return redirect(url_for('login'))
 
-    else:
-        flash('Usuário não logado')
-        return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
